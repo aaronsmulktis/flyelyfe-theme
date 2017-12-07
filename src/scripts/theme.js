975333e31fa283b051ed8f27db32387d313ad1fd
Aaron Smulktis <aaronsmulktis@gmail.com>
2017-12-06 21:44:20 -0800

    show email bar on every page, tweak email bar style, fix extra liquid closing tag on theme.liquid, scroll to top on gumbit click, new font slightly smaller for product title, MAKE SLIDER AWESOME, fix image height in product grid

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -92,6 +92,12 @@ $(document).ready(function() {
     }]
   });
 
+  // Interactions
+
+  $('.newsletter-banner-image').on('click touchstart', function() {
+    $("html, body").animate({scrollTop: 0}, 'fast');
+    return false;
+  });
 
   // Defaults
 
